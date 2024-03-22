b52a03a531287cdca4eb2b443bafd8c8a4ab74c6
bwhite <1240160064@qq.com>
2024-03-22 21:31:15 +0800

    nothing

---
+++ day01_txlu/cmake-build-debug/CMakeFiles/day01.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/b_white/GitHub/Cpp_CODE/day01_txlu/main.cpp" "CMakeFiles/day01.dir/main.cpp.o" "gcc" "CMakeFiles/day01.dir/main.cpp.o.d"
+  "/Users/b_white/GitHub/Cpp_CODE/day01_txlu/utils.cpp" "CMakeFiles/day01.dir/utils.cpp.o" "gcc" "CMakeFiles/day01.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
